0f6ad8bab78d348be56325dbf1fd80ff49c67b99
goeun <wjdrhdms002@naver.com>
2022-11-07 22:56:01 +0900

    Impl sub menu contents

--- components/sub-menu.tsx
+++ components/sub-menu.tsx
@@ -1,4 +1,25 @@
 import { css } from '@emotion/react'
+import Image from 'next/image'
+import searchIcon from '../static/icons/search-icon.svg'
+import profileImg from '../static/images/profile-img.jpeg'
+
+interface CategoryType {
+  name: string
+  cnt: number
+}
+
+const categoryDummy: Array<CategoryType> = [
+  { name: 'Web', cnt: 1 },
+  { name: 'Vue', cnt: 1 },
+  { name: 'Typescript', cnt: 1 },
+  { name: 'Javascript', cnt: 1 },
+  { name: 'Network', cnt: 1 },
+  { name: '회고', cnt: 1 },
+  { name: '에러', cnt: 1 },
+  { name: 'cors', cnt: 1 },
+  { name: 'Recoil', cnt: 1 },
+  { name: 'css', cnt: 1 },
+]
 
 const SubMenu = () => {
   return (
@@ -6,13 +27,61 @@ const SubMenu = () => {
       className="sub-menu-container"
       css={css`
         width: 20%;
-        padding: 2rem;
+        padding: 1.5rem;
         border-right: #92999c 1px solid;
       `}
     >
-      <div>검색 영역</div>
-      <div>프로필 영역</div>
-      <div>카테고리 영역</div>
+      <div className="flex items-center justify-between mb-8">
+        <div className="w-11/12 border-b-2">
+          <input
+            css={css`
+              width: 95%;
+              height: 2.3rem;
+              border-radius: 5px;
+              padding: 0 0.6rem;
+            `}
+            placeholder="Search"
+          ></input>
+        </div>
+        <div className="rounded-xl">
+          <Image src={searchIcon} alt="search-image" />
+        </div>
+      </div>
+      <div className="pb-10 profile-contaniner">
+        <Image
+          src={profileImg}
+          style={{ borderRadius: '10px' }}
+          alt="profile-image"
+        />
+        <div className="my-4">
+          <div className="mb-2 text-3xl">Just Do it !</div>
+          <div className="text-xl">박치기 전문 개발자, 💻 🐱 ✍️ </div>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="text-2xl border-b-[1px] border-solid border-slate-400">
+          카테고리
+        </div>
+        <div>
+          <ul>
+            {categoryDummy.map((d, idx) => {
+              return (
+                <li
+                  css={css`
+                    font-size: 18px;
+                    cursor: pointer;
+                    margin: 1rem 0rem;
+                    :hover {
+                      text-decoration: underline;
+                    }
+                  `}
+                  key={`category-list-${d.name}`}
+                >{`${d.name} (${d.cnt})`}</li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
