6486d61492f1bfff9b259dc1b0a7c9a7c8574b79
goeun <wjdrhdms002@naver.com>
2022-11-14 15:29:21 +0900

    Reconstruct prisma db model

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import styled from '@emotion/styled'
 import type { AppProps } from 'next/app'
 
-export default function About({ Component, pageProps }: AppProps) {
-  return <div>About</div>
+const AboutContaier = styled.div``
+const About = ({ Component, pageProps }: AppProps) => {
+  return (
+    <AboutContaier>
+      <div>이미지 사진</div>
+      <div>간략한 소개</div>
+      <div>깃헙, 노션, 메일</div>
+
+      <div>Skil Set</div>
+    </AboutContaier>
+  )
 }
+
+export default About
